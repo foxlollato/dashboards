1ff4ad16c374f3a3c94e74336304673480ab02d5
foxlollato <vitor.lollato@gmail.com>
2020-04-02 10:22:12 -0300

    removed scroll from iframe

--- src/charts.js
+++ src/charts.js
@@ -6,69 +6,68 @@ import { Chart } from 'chart.js';
 
 window.addEventListener('load', run);
 
-function run(){
-  chart1();
-  chart2();
+async function  run(){
+  const data = await getData();
+  chart1(data);
+  chart2(data);
 };
   
   
-  async function chart1() {
-      const ctx = document.getElementById('chart1').getContext('2d');
-      const data = await getData();
-      const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: data.name,
-          datasets: [
-            {
-              label: 'Lines of Code',
-              data: data.loc,
-              fill: false,
-              borderColor: 'rgba(255, 99, 132, 1)',
-              backgroundColor: 'rgba(255, 99, 132, 0.5)',
-              borderWidth: 1
-            }
-          ]
-        },
-        options: {}
-      });
-    };
+function chart1(data) {
+  const ctx = document.getElementById('chart1').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: data.name,
+      datasets: [
+        {
+          label: 'Lines of Code',
+          data: data.loc,
+          fill: false,
+          borderColor: 'rgba(255, 99, 132, 1)',
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {}
+  });
+};
 
-    async function chart2() {
-      const ctx = document.getElementById('chart2').getContext('2d');
-      const data = await getData();
-      const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: data.name,
-          datasets: [
-            {
-              label: 'Commits',
-              data: data.commit,
-              fill: false,
-              borderColor: 'rgba(0, 99, 132, 1)',
-              backgroundColor: 'rgba(0, 99, 132, 0.5)',
-              borderWidth: 1
-            }
-          ]
-        },
-        options: {}
-      });
-    };
+function chart2(data) {
+  const ctx = document.getElementById('chart2').getContext('2d');
+  const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: data.name,
+      datasets: [
+        {
+          label: 'Commits',
+          data: data.commit,
+          fill: false,
+          borderColor: 'rgba(0, 99, 132, 1)',
+          backgroundColor: 'rgba(0, 99, 132, 0.5)',
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {}
+  });
+};
 
-    async function getData() {
-      const response = await fetch('commits.csv');
-      const data = await response.text();
-      const loc = [];
-      const commit = [];
-      const name = [];
-      const rows = data.split('\n').slice(1);
-      rows.forEach(row => {
-        const cols = row.split(',');
-        name.push(cols[0]);
-        loc.push(cols[1]);
-        commit.push(cols[2]);
-        
-      });
-      return { loc, commit, name };
-    };
+async function getData() {
+  const response = await fetch('commits.csv');
+  const data = await response.text();
+  const loc = [];
+  const commit = [];
+  const name = [];
+  const rows = data.split('\n').slice(1);
+  
+  rows.forEach(row => {
+    const cols = row.split(',');
+    name.push(cols[0]);
+    loc.push(cols[1]);
+    commit.push(cols[2]);    
+  });
+  return { loc, commit, name };
+};
